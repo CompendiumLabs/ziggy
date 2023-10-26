fb0f855e66361fd110f1ffe859efd74a89dfd135
Douglas Hanley <thesecretaryofwar@gmail.com>
2023-10-25 22:30:59 -0400

    benchmark aggregation

--- bench.py
+++ bench.py
@@ -2,14 +2,24 @@
 
 import os
 import time
+import json
 from itertools import chain
 from subprocess import run
+from glob import glob
 
 import torch
 from mteb import MTEB
 
 from llm import HuggingfaceEmbedding
 from database import stream_jsonl, paragraph_splitter, DocumentDatabase
+
+TASKS_RETRIEVAL = [
+    'ArguAna', 'ClimateFEVER', 'CQADupstackAndroidRetrieval', 'CQADupstackEnglishRetrieval', 'CQADupstackGamingRetrieval',
+    'CQADupstackGisRetrieval', 'CQADupstackMathematicaRetrieval', 'CQADupstackPhysicsRetrieval', 'CQADupstackProgrammersRetrieval',
+    'CQADupstackStatsRetrieval', 'CQADupstackTexRetrieval', 'CQADupstackUnixRetrieval', 'CQADupstackWebmastersRetrieval',
+    'CQADupstackWordpressRetrieval', 'DBPedia', 'FEVER', 'FiQA2018', 'HotpotQA', 'MSMARCO', 'NFCorpus', 'NQ', 'QuoraRetrieval',
+    'SCIDOCS', 'SciFact', 'Touche2020', 'TRECCOVID'
+]
 
 # load jsonl chunks directly
 def load_data(path, delim='\n', minlen=100, nrows=None):
@@ -65,7 +75,7 @@ class MtebWrapper:
             vec1 = self.qspec.dequantize(qvec)
         return vec1.cpu().numpy()
 
-def profile_mteb(model, maxlen=256, onnx=True, compile=False):
+def profile_mteb(model, maxlen=256, savedir='benchmarks', onnx=True, compile=False):
     if type(model) is str:
         emb = HuggingfaceEmbedding(model_id=model, maxlen=maxlen, onnx=onnx, compile=compile)
     else:
@@ -73,11 +83,48 @@ def profile_mteb(model, maxlen=256, onnx=True, compile=False):
     emb1 = MtebWrapper(emb)
 
     # run benchmarks
-    evaluation = MTEB(task_types=['Retrieval'])
-    results = evaluation.run(emb1, output_folder=f'benchmarks/{model}-{maxlen}')
+    evaluation = MTEB(tasks=TASKS_RETRIEVAL, task_langs='en')
+    results = evaluation.run(emb1, output_folder=f'{savdir}/{model}-{maxlen}')
 
-    # print our results
-    print(results)
+def path_to_spec(path):
+    org, tag = path.split('/')
+    return org, *tag.rsplit('-', maxsplit=2)
+
+def extract_json(path, keys):
+    name, _ = os.path.splitext(path)
+    with open(path) as f:
+        data = json.load(f)
+    for k in keys:
+        data = data[k]
+    return data
+
+def aggregate_mteb(savedir='benchmarks', metric='ndcg_at_10'):
+    import numpy as np
+    import pandas as pd
+
+    # get model specs
+    models = glob(f'*/*', root_dir=savedir)
+    specs = [path_to_spec(path) for path in models]
+
+    # get benchmark results
+    benchs = [[
+        os.path.splitext(b)[0] for b in glob('*.json', root_dir=f'{savedir}/{p}')
+    ] for p in models]
+    results = [[
+        (b, extract_json(f'{savedir}/{m}/{b}.json', ('test', metric))) for b in bs
+    ] for m, bs in zip(models, benchs)]
+
+    # combine results
+    info = chain(*[[(*m, *b) for b in bs] for m, bs in zip(specs, results)])
+
+    # return dataframe
+    columns = ['org', 'model', 'maxlen', 'quant']
+    data = pd.DataFrame.from_records(info, columns=[*columns, 'bench', metric])
+    data[metric] = 100*data[metric].astype(np.float32)
+    data = data.sort_values(by=columns)
+    avgs = data.groupby(columns)[metric].mean().to_frame()
+
+    return data, avgs
 
 # main entry point
 if __name__ == '__main__':
